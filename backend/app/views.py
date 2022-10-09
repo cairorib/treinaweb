0b8f47c081418cc05ea208ebc58e9beb8d1ce54e
cairo <cairoribeiro@hotmail.com>
2022-10-09 15:12:41 -0300

    envio 1

---
+++ backend/app/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HomeApiView(APIView):
+    def get(self,request, format=None):
+        return Response(
+            {
+                "nome":"Cairo Ribeiro",
+                "idade":"41"
+            }, 
+            status=200
+)

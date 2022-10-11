dfb64ba0a45de831bb0d868ca2bc385fc272cec7
cairo <cairoribeiro@hotmail.com>
2022-10-11 17:50:33 -0300

    envio1

---
+++ frontend/pages/contato.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Contato: NextPage =()=>{
+    return (
+        <div>
+            contato123
+        </div>
+    )
+}
+
+export default Contato;
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Header from "../src/components/Header/Header";
+
+/* 
+function Botao(props): JSX.Element{
+ return  <a href="#">{props.title}</a>
+} */
+
+const home: NextPage =()=>{
+    return (
+    <>     
+      <p>
+      index
+      </p>
+    
+    </>
+
+        
+    )
+}
+
+export default home;
